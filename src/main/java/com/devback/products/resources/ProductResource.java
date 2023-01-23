7b5b20eb9a36481ad8a0f358376da0b153936fcd
DevBack <heltonlima.ti@live.com>
2023-01-23 01:58:47 -0300

    Update ProductResourse Class

--- src/main/java/com/devback/products/resources/ProductResource.java
+++ src/main/java/com/devback/products/resources/ProductResource.java
@@ -1,8 +1,10 @@
 package com.devback.products.resources;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devback.products.entities.Product;
+import com.devback.products.resources.exceptions.StandardError;
 import com.devback.products.services.ProductService;
+import com.devback.products.services.exceptions.EntityNotFoundException;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -21,13 +25,15 @@ public class ProductResource {
 	
 	@GetMapping
 	public ResponseEntity<List<Product>> findAll() {
-		List<Product> list = productService.findAll();
-		return ResponseEntity.ok().body(list);
+		
+			List<Product> list = productService.findAll();
+			return ResponseEntity.ok().body(list);
 	}
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> findById(@PathVariable Long id) {
-		Product category = productService.findById(id);
-		return ResponseEntity.ok().body(category);
+		
+			Product category = productService.findById(id);
+			return ResponseEntity.ok().body(category);
 	}
 }
